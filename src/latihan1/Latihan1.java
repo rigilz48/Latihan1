643984ced18fc2ddc1e1492761530122a2392df0
Rigil Makmun <44825015+rigilz48@users.noreply.github.com>
2018-11-13 18:35:52 +0700

    Add files via upload

---
+++ src/latihan1/Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author 100 Mbps
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Menampilkan
+        System.out.println("Saya belajar pemrograman GUI menggunakan Java");
+    }
+    
+}
